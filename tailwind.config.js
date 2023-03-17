45ad6ca29ea5ade032e6c36092411997384f68f5
Shivam Kataria <114555700+shivamkataria13@users.noreply.github.com>
2023-03-17 20:41:08 +0530

    opening page finished

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 'D:\coding\practice\projects\portfolio-\index.html',
+              "./**/*.{html,js}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
