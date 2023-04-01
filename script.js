c76f986b5d3bf2e72795f501782ed9f95826b9cd
Shivam Kataria <114555700+shivamkataria13@users.noreply.github.com>
2023-04-01 23:33:24 +0530

    r

--- script.js
+++ script.js
@@ -31,6 +31,27 @@ buttontwo.addEventListener('click', ()=>{
 
 
 
+// Opening Page button
+const opBtn = document.querySelector('.opButton')
+const header = document.querySelector('.header')
+const openingPage = document.querySelector('.openingPage')
+const footer = document.querySelector('footer')
+const mainPage = document.querySelector('.main')
+
+
+opBtn.addEventListener('click', ()=>{
+
+    header.classList.remove('hidden')
+    footer.classList.remove('hidden')
+
+    mainPage.classList.remove('hidden')
+
+    openingPage.classList.add('hidden')
+    openingPage.classList.add('openingPageAnimation')
+
+})
+
+
 
 
 
@@ -41,3 +62,30 @@ buttontwo.addEventListener('click', ()=>{
 //     blur.classList.toggle('blurbg')
 // }
 
+
+
+
+
+
+// sections scroll animation
+
+const sections = document.querySelectorAll('.sections')
+
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        entry.target.classList.add('show')
+        // entry.target.classList.add('animate__animated', entry.isIntersecting)
+        // entry.target.classList.add('animate__fadeIn', entry.isIntersecting)
+        // entry.target.classList.add('animate__delay-2s', entry.isIntersecting)
+    })
+    console.log(entries)
+},{
+    threshold:.9,
+    // rootMargin: '-20px',
+    root: document.querySelector('.main'),
+})
+
+
+sections.forEach(section=>{
+    observer.observe(section);
+})
